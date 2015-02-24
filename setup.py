048966eaaa02b88051d50113c32f266bf28e9752
Yoann Moreau <yoann.moreau@gmail.com>
2015-02-24 18:37:37 +0100

    installation

--- setup.py
+++ setup.py
@@ -48,7 +48,6 @@ setup(name="landsat_theia",
       license="CCO",
       platforms="Posix; ",
       install_requires=[
-          "osgeo",
           "GDAL==1.11.0",
           "python-dateutil==2.2",
           "numpy"
